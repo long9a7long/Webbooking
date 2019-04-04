c0fbe53a6da905fbbcb9f5c6d01f8c3ed2af0f64
IT2019 <0510825089son@gmail.com>
2019-04-04 18:38:01 +0700

    up trang hotel by son

--- application/controllers/hotel.php
+++ application/controllers/hotel.php
@@ -10,17 +10,32 @@ class Hotel extends CI_Controller {
 
 	public function index()
 	{
+
         $this -> data['title'] = "Booking Hotel";  
-        $this -> data['slide'] = "default/hotel/slide"; 
+        $this -> data['after_header'] = "default/hotel/slide"; 
         $this -> data['temp'] = "default/hotel/hotel";
-        
+        $this -> data['custom_js'] = array(
+            
+            "assets/default/js/map_hotels.js","assets/default/js/functions.js");
+            $this -> data['custom_js_external'] = array(
+                "http://maps.googleapis.com/maps/api/js");
         $this->load->view("default/template",$this ->data);
     }
 
     public function detail()
     {
-        $this -> data['title'] = "Booking Hotel";
+        $this -> data['title'] = "Single Hotel";
+        $this -> data['after_header']="default/hotel/hotel-detail-slide";
         $this -> data['temp'] = "default/hotel/hotel-detail";
+        $this -> data['custom_js_external'] = array(
+            "https://maps.googleapis.com/maps/api/js");
+        $this -> data['custom_js'] = array(
+            
+            "assets/default/js/jquery.sliderPro.min.js",
+            "assets/default/js/map.js",
+            "assets/default/js/infobox.js",
+            "assets/default/assets/validate.js",
+            "assets/default/js/hotel-detail.js");
         
         $this->load->view("default/template",$this ->data);
     }
